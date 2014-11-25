704dd5f457a1a76574c4da64b94d00bd38f1e4e8
Jhonatan <Jhonatan@10.10.27.178>
2014-11-25 10:55:56 -0200

    First commit

---
+++ src/main/java/Pratica52.java
@@ -0,0 +1,24 @@
+
+import utfpr.ct.dainf.if62c.pratica.Equacao2Grau;
+
+/**
+ * UTFPR - Universidade Tecnológica Federal do Paraná
+ * DAINF - Departamento Acadêmico de Informática
+ * IF62C - Fundamentos de Programação 2
+ * 
+ * Template de projeto de programa Java usando Maven.
+ * @author Wilson Horstmeyer Bogado <wilson@utfpr.edu.br>
+ */
+public class Pratica52 {
+    public static void main(String[] args) {
+        Equacao2Grau<Integer> e1 = new Equacao2Grau<>(7,8,1);
+        Equacao2Grau<Integer> e2 = new Equacao2Grau<>(1,2,1);
+        Equacao2Grau<Integer> e3 = new Equacao2Grau<>(0,1,1);
+        System.out.println(e1.getRaiz1());
+        System.out.println(e1.getRaiz2());
+        System.out.println(e2.getRaiz1());
+        System.out.println(e2.getRaiz2());
+        //System.out.println(e3.getRaiz1());
+        //System.out.println(e3.getRaiz2());
+    }
+}
---
+++ src/main/java/utfpr/ct/dainf/if62c/pratica/Equacao2Grau.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utfpr.ct.dainf.if62c.pratica;
+
+/**
+ *
+ * @author Jhonatan
+ * @param <N>
+ */
+public class Equacao2Grau<N extends Number> {
+    N a;
+    N b;
+    N c;
+
+    public Equacao2Grau (N a, N b, N c) throws RuntimeException {
+        if(a.intValue() == 0) throw new RuntimeException("Coeficiente nao pode ser zero.");
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public N getA() {
+        return a;
+    }
+
+    public void setA(N a) throws RuntimeException {
+        if(a.intValue() == 0) throw new RuntimeException("Coeficiente nao pode ser zero.");
+        else this.a = a;
+    }
+
+    public N getB() {
+        return b;
+    }
+
+    public void setB(N b) {
+        this.b = b;
+    }
+
+    public N getC() {
+        return c;
+    }
+
+    public void setC(N c) {
+        this.c = c;
+    }
+    
+    public double getRaiz1() throws RuntimeException
+    {
+        double raiz1 = b.doubleValue()*b.doubleValue() - 4*a.doubleValue()*c.doubleValue();
+        if( raiz1 < 0) throw new RuntimeException("Sem solução real");
+        
+        else raiz1 = Math.sqrt(raiz1);
+        raiz1 = -1.0*b.doubleValue() + raiz1;
+        raiz1 = raiz1/(2*a.doubleValue());
+        return raiz1;
+    }
+    
+    public double getRaiz2 () throws RuntimeException
+    {
+        double raiz2 = b.doubleValue()*b.doubleValue() - 4*a.doubleValue()*c.doubleValue();
+        if( raiz2 < 0) throw new RuntimeException("Sem solução real");
+        
+        else raiz2 = Math.sqrt(raiz2);
+        raiz2 = -1.0*b.doubleValue() - raiz2;
+        raiz2 = raiz2/(2*a.doubleValue());
+        return raiz2;
+    }
+    
+}
